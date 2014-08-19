b63b8b0c2d34bbf8990b735d02154dbc9f94aab7
Guillermo Maschwitz <guillermo@streamlinesocial.com>
2014-08-19 17:35:41 -0300

    fixing indentation, removing debug lines

--- Resources/public/js/l10n.js
+++ Resources/public/js/l10n.js
@@ -2,10 +2,10 @@
  * namespaces
  */
 var StrSocialL10n = {
-		fn: {},
-		api: {
-			timezone: {}
-		}
+	fn: {},
+    api: {
+		timezone: {}
+    }
 };
 
 
@@ -15,18 +15,16 @@ var StrSocialL10n = {
 StrSocialL10n.api.timezone.post = function( tz, successCallback, errorCallback ){
 	console.log('updating remote timezone...');
 	$.ajax({
-        type: "POST",
-        url: location.protocol + '//' + location.host + "/app_dev.php/l10n/gmt-offset",
-        data: { timezone: tz },
-        success: function(rsp){
-        	console.log('post success!', rsp);
-        	successCallback( rsp );
-        },
-        error: function(rsp){
-        	console.log('post failed!', rsp);
-        	errorCallback(rsp);
-        },
-    });
+		type: "POST",
+		url: location.protocol + '//' + location.host + "/app_dev.php/l10n/gmt-offset",
+		data: { timezone: tz },
+		success: function(rsp){
+			successCallback( rsp );
+		},
+		error: function(rsp){
+			errorCallback(rsp);
+		},
+	});
 }
 
 
@@ -36,29 +34,24 @@ StrSocialL10n.api.timezone.post = function( tz, successCallback, errorCallback )
  */
 StrSocialL10n.fn.postTimeZoneIfNeeded = function(){
 	console.log('checking remote timezone...');
-	
-    $.ajax({
-        type: "GET",
-        url: location.protocol + '//' + location.host + "/app_dev.php/l10n/timezone",
-        success: function(rsp){
-        	console.log('get success!', rsp);
-        },
-        error: function(rsp){
-        	console.log('get failed!', rsp);
-        	var visitortime = new Date();
-            var visitortimezone = -visitortime.getTimezoneOffset()/60;
-        	StrSocialL10n.api.timezone.post( visitortimezone, function(){
-        		// on success reload the page
-        			console.log('post success');
-	        		location.reload();
-        		}, function(){
-	        	//error callback
-        			console.log('post failed! Retrying...');
-	        		StrSocialL10n.fn.postTimeZoneIfNeeded();
-        		}
-        	);
-        }
-    });
+
+	$.ajax({
+		type: "GET",
+		url: location.protocol + '//' + location.host + "/app_dev.php/l10n/timezone",
+		success: function(rsp){
+		},
+		error: function(rsp){
+			var visitortime = new Date();
+			var visitortimezone = -visitortime.getTimezoneOffset()/60;
+			StrSocialL10n.api.timezone.post( visitortimezone, function(){
+				// on success reload the page
+					location.reload();
+			}, function(){
+				//error callback
+				StrSocialL10n.fn.postTimeZoneIfNeeded();
+			});
+		}
+	});
 };
 
 
