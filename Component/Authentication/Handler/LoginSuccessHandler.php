7c223336af7e1538aef987d293f66104d7ed9704
Guillermo Maschwitz <guillermo@streamlinesocial.com>
2014-08-20 15:04:29 -0400

    draft of an authentication handler to set timezone into session at signin

---
+++ Component/Authentication/Handler/LoginSuccessHandler.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace StrSocial\Bundle\LocalizationBundle\Component\Authentication\Handler;
+
+use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Router;
+
+
+/**
+ * draft of an authentication handler to set timezone into session at signin
+ */
+class LoginSuccessHandler implements AuthenticationSuccessHandlerInterface
+{
+
+    protected $router;
+    protected $security;
+
+    public function __construct(Router $router, SecurityContext $security)
+    {
+        $this->router = $router;
+        $this->security = $security;
+    }
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token)
+    {
+    }
+
+}
